3ae68174257c83ea15c5828a5849ec766d852e3b
X-merc <mayan.mehra2012@gmail.com>
2024-11-02 15:50:46 +0530

    migrations

---
+++ src/visits/models.py
@@ -0,0 +1,7 @@
+from django.db import models
+
+# Create your models here.
+class page_visits(models.Model):
+    path = models.TextField(blank= True, null = True)
+    timestamp = models.DateTimeField(auto_now_add=True)
+
